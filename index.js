d48940aaaa1f82258a001c6ca074e5e8d2e7851b
roshanr <roshanranabhat11@gmail.com>
2020-11-08 08:07:49 +0545

    Rest route explained

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-console.log('this is express js')
+// let blog = {
+// 	id: '',
+// 	title: '',
+// 	author: '',
+// 	userId: '',
+// 	created_at: '',
+// 	updated_at: ''
+// }
+
+// blogs = [{
+// 	id: '1',
+// 	title: 'hello',
+// 	author: 'ram',
+// 	userId: '1',
+// 	created_at: '2020-12-12',
+// 	updated_at: ''
+// },
+// {
+// 	id: '2',
+// 	title: 'world',
+// 	author: 'shyam',
+// 	userId: '2',
+// 	created_at: '',
+// 	updated_at: ''
+// },
+// {
+// 	id: '3',
+// 	title: 'iims',
+// 	author: 'hari',
+// 	userId: '3',
+// 	created_at: '',
+// 	updated_at: ''
+// }]
 
 
+
+REST API
+Routes      |   RequestMethod  |    Description
+/blog           GET                 (query get all blogs);
+/blog           POST                (Store a blog)
+/blog/:id       PUT/PATCH           (Update a blog)
+/blog/:id       DELETE              (Delete a blog with id)
+/blog/:id       GET                 (Get a blog by id)
