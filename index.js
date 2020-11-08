948e118ac873651c44745507d1307598ca046be4
roshanr <roshanranabhat11@gmail.com>
2020-11-08 08:57:55 +0545

    Express create REST routes

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-console.log('this is express js')
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+let blogs = [{
+	id: 1,
+	title: 'hello'
+},
+{
+	id: 2,
+	title: 'world'
+}];
 
 
+app.get('/blog',(req, resp) => {
+	resp.json(blogs);
+});
+
+app.post('/blog',(req, res) => {
+	res.json({method: req.method});
+});
+
+app.put('/blog/:id',(req, res) => {
+	res.json({method: req.method});
+});
+
+app.get('/blog/:id', (req, res) => {
+	let data = blogs.filter((value) => value.id === parseInt(req.params.id));
+	if(!data[0]){
+		return res.status(404).json({error: 'Data not found'});
+	}
+	res.json(data[0]);
+})
+
+app.delete('/blog/:id', (req, res) => {
+	res.json({method: req.method});
+})
+
+app.listen(port, () => {
+	console.log(`Application is running on port: ${port}`);
+})
